103fb3a9d19bd404c7715b3ed3778c23a92bfd0e
manvi0308 <chaddhamanvi3@gmail.com>
2024-11-01 15:18:02 +0530

    Added problem 270

--- Problem270.java
+++ Problem270.java
@@ -1,4 +1,6 @@
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Employee {
     int id;
@@ -7,7 +9,23 @@ class Employee {
 }
 
 public class Problem270 {
+    Map<Integer, Employee> eMap;
     public int getImportance(List<Employee> employees, int id) {
-        
+        eMap = new HashMap<>();
+        for(Employee e : employees){
+            eMap.put(e.id, e);
+        }
+
+        return dfs(id);
+    }
+
+    public int dfs(int id){
+        Employee employee = eMap.get(id);
+        int ans = employee.importance;
+        for(Integer it : employee.subordinates){
+            ans += dfs(it);
+        }
+
+        return ans;
     }
 }
