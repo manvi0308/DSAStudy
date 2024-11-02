6ff4ac2da54f5615f8fdc16d9de9d432039b2e35
manvi0308 <chaddhamanvi3@gmail.com>
2024-11-02 08:01:51 +0530

    added program 3334

---
+++ Problem3334.java
@@ -0,0 +1,9 @@
+public class Problem3334 {
+    public long maxScore(int[] nums){
+        /*
+         * product of lcm and gcd of all elements
+         * return the maximum factor score after removing at most 
+         * one element
+         */
+    }
+}
